991d94832814421cf696ae40f23117783646388a
clamascabaleiro <clamascabaleiro@nautilus15.danielcastelao.org>
2019-05-20 12:31:10 +0200

    Insertamos un registro en la base de datos "alumno.db" añadiendo un alumno a la tabla dam1

---
+++ Sqlite/src/sqlite/classInsertTable.java
@@ -0,0 +1,39 @@
+package sqlite;
+
+import java.sql.*;
+
+/**
+ *
+ * @author clamascabaleiro
+ */
+public class classInsertTable {
+    public static class InsertApp {
+    public Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:alumno.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+ 
+    public void insert(int numero, String nombre, float nota) {
+        String sql = "INSERT INTO dam1(nº ,nombre, nota) VALUES(?,?,?)";
+ 
+        try (Connection conn = this.connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, numero);
+            pstmt.setString(2, nombre);
+            pstmt.setFloat(3,nota);
+            pstmt.executeUpdate();
+            System.out.println("Insertado");
+        } catch (SQLException e) {
+            System.out.println("Registro no insertado no encuentra la tabla");
+        }
+    }
+ 
+}
+}
